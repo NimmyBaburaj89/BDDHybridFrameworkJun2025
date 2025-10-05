dcaebfa5fb05622974616a15fa3306fbc2977447
NimmyBaburaj89 <vipin86805@gmail.com>
2025-10-05 07:48:28 +0400

    Updated runner class to run only buzz page

--- src/test/java/com/qa/runner/Runner.java
+++ src/test/java/com/qa/runner/Runner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features", glue = { "com.qa.stepdefinations" }, tags = {
-		"@executeallfromJenkins" }, plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json" }, monochrome = true)
+		"@BuzzCRUDTest" }, plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json" }, monochrome = true)
 
 //html:target/cucumber-reports
 
